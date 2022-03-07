d25f622b7e874c1052208ead76f63aabd153d6e9
xushengfeng <xushengfeng_zg@163.com>
2022-03-07 23:30:51 +0800

    :zap:保存到缓冲区

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include "widget.h"
 
 #include <QApplication>
-#include <QScreen>
+#include <QBuffer>
 #include <QDebug>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
 
-    QList<QScreen *> screen_list= QGuiApplication::screens();
-       for(int i=0;i<screen_list.count();i++)
-       {
-           qDebug()<<screen_list.at(i)->geometry();
-           screen_list.at(i)->grabWindow(0).save(QString::number(i),"png");
-       }
-//    w.show();
+    QList<QScreen *> screen_list = QGuiApplication::screens();
+    for (int i = 0; i < screen_list.count(); i++) {
+        qDebug() << screen_list.at(i)->geometry();
+        QPixmap pixmap;
+        QByteArray bytes;
+        QBuffer buffer(&bytes);
+        buffer.open(QIODevice::WriteOnly);
+        screen_list.at(i)->grabWindow(0).save(&buffer, "png");
+    }
+    //    w.show();
     return a.exec();
 }
