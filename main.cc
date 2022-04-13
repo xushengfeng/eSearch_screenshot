dcbd2a9844c5d2769a7585c8a15bafdbd95e4215
xushengfeng <xushengfeng_zg@163.com>
2022-04-13 23:40:09 +0800

    :sparkles:根据系统执行不同代码

--- main.cc
+++ main.cc
@@ -1,4 +1,6 @@
 #include <node.h>
+#include <nan.h>
+#include <v8.h>
 
 namespace demo
 {
@@ -6,6 +8,7 @@ namespace demo
 	using v8::FunctionCallbackInfo;
 	using v8::Isolate;
 	using v8::Local;
+	using v8::Number;
 	using v8::Object;
 	using v8::String;
 	using v8::Value;
@@ -14,6 +17,22 @@ namespace demo
 	void Method(const FunctionCallbackInfo<Value> &args)
 	{
 		Isolate *isolate = args.GetIsolate();
+
+		double p = args[0].As<Number>()->Value();
+
+		if (p == 0) //"win"
+		{
+		}
+		else if (p == 1) //"x11"
+		{
+		}
+		else if (p == 2) //"wayland"
+		{
+		}
+		else if (p == 3) //"mac"
+		{
+		}
+
 		args.GetReturnValue().Set(args[0]);
 	}
 
