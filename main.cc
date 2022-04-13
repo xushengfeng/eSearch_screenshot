d82e69345ef1b249143413bd99f9eac8ee619960
xushengfeng <xushengfeng_zg@163.com>
2022-04-13 21:20:59 +0800

    :sparkles:传入系统参数

--- main.cc
+++ main.cc
@@ -1,26 +1,27 @@
 #include <node.h>
 
-namespace demo {
+namespace demo
+{
 
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::Object;
-using v8::String;
-using v8::Value;
+	using v8::FunctionCallbackInfo;
+	using v8::Isolate;
+	using v8::Local;
+	using v8::Object;
+	using v8::String;
+	using v8::Value;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(
-      isolate, "world").ToLocalChecked());
-}
+	// 传入系统类型参数
+	void Method(const FunctionCallbackInfo<Value> &args)
+	{
+		Isolate *isolate = args.GetIsolate();
+		args.GetReturnValue().Set(args[0]);
+	}
 
-void Initialize(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "hello", Method);
-}
+	void Initialize(Local<Object> exports)
+	{
+		NODE_SET_METHOD(exports, "capture", Method);
+	}
 
-NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+	NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
 
-}
-
-
+} // namespace demo
--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 const ss = require("./build/Release/eSearch_screenshot");
+console.log(ss.capture("linux"));
